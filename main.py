78b9436c67ece6341efaa387220c179ccba0366c
Cameron <cameron.smith2814@gmail.com>
2025-11-05 14:15:51 +0000

    Design pivot due to lack of js support

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
+import json
 app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return render_template('index.html')
+    retrieveData()
+    return render_template('index.html', title=retrieveData())
+
+import requests
+
+def retrieveData():
+    url = "https://comicvine.gamespot.com/api/"
+    req = "https://comicvine.gamespot.com/api/search/?api_key=2b739459da8dc4ec62f68656b642554dea026eca&format=json&resources=character&query=Superman"
+    headers = {
+        "Content-Type":"application/json"
+    }
+    response = requests.get(req)
+    #data = response.json()
+    return response
